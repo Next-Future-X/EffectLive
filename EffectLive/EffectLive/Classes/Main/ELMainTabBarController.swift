a4731c58fb1fd6d5da4fbf7ddf9bb00c2e5f4574
yuanxiangyang0528 <yuanxiangyang0528@163.com>
2016-12-07 17:53:42 +0800

    添加了appIcon和launchImage,创建了storyboard

---
+++ EffectLive/EffectLive/Classes/Main/ELMainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  ELMainTabBarController.swift
+//  EffectLive
+//
+//  Created by 袁向阳 on 16/12/7.
+//  Copyright © 2016年 YXY.cn. All rights reserved.
+//
+
+import UIKit
+
+class ELMainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    fileprivate func addChilederS() {
+        
+    }
+    fileprivate func setUpChildController(name:String) {
+        
+    }
+}
